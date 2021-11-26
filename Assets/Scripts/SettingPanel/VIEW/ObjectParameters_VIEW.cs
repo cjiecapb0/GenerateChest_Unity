fe290479741db1f462532482d11505c61db9d653
Sergei Shimanski <s.shimanski@totalgames.io>
2021-11-26 19:15:43 +0300

    add

---
+++ Assets/Scripts/SettingPanel/VIEW/ObjectParameters_VIEW.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectParameters_VIEW : MonoBehaviour
+{
+
+
+
+    public ObjectParameters_VIEW(Dictionary<string, List<GameObject>> prefabsList)
+    {
+        //this.togglesCountResources = prefabsList.ContainsKey("togglesCountResources")
+        //    ? prefabsList["togglesCountResources"]
+         //   : null;
+
+
+    }
+
+}
